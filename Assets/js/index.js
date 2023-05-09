09214cc3c0410dcd2ae15afc9fa137a49ef6abca
Kanayo <anayoifediora@live.com>
2023-05-09 16:43:45 +0930

    obtained one day forecast weather info

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -8,30 +8,69 @@ var currentDate = document.querySelector('.date');
 var form = document.querySelector('form');
 var formInputEl = document.querySelector('.form-control');
 
+// Dom elements for the 5-day forecast
+var forecastDate = document.querySelector('.card-title')
+var nameOfCity = document.querySelector('.card-subtitle');
+var forecastIcon = document.querySelector('.forecast-icon');
+var forecastDescription = document.querySelector('.forecast-description');
+var forecastTemperature = document.querySelector('.temperature');
+var forecastWindSpeed = document.querySelector('.wind');
+var forecastHumidity = document.querySelector('.humidity');
+
 // currentDate.textContent = dayjs().format('DD/MM/YYYY');
 
 let cityName = formInputEl.value.trim();
 
 function getCurrentWeather(cityName) {
 
-var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=443ce9d7f610c028f54310f3c2bc25fd'
+let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=443ce9d7f610c028f54310f3c2bc25fd'
 
     fetch(requestUrl)
     .then(function (response) {
     return response.json();
     })
     .then(function (data) {
-    console.log(data);
+    // console.log(data);
     currentCity.textContent = data.name;
     weatherImageDescription.textContent = data.weather[0].description;
-    currentTemperature.textContent = "Temperature: " + Math.ceil(data.main.temp) + " Celsius";
+    currentTemperature.textContent = "Temperature: " + Math.floor(data.main.temp) + " °C";
     currentWindSpeed.textContent = "Wind Speed: " + data.wind.speed;
-    currentHumidity.textContent = "Humidity: " + data.main.humidity;
+    currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
     weatherImage.setAttribute("src", 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
     }
     );
 
+    let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric&appid=443ce9d7f610c028f54310f3c2bc25fd'
+
+    fetch(forecastUrl)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    console.log(data);
+
+        forecastDate.textContent = data.list[7].dt_txt;
+        nameOfCity.textContent = cityName;
+        forecastIcon.setAttribute('src', 'https://openweathermap.org/img/w/' + data.list[7].weather[0].icon + '.png');
+        forecastDescription.textContent = data.list[7].weather[0].description;
+        forecastTemperature.textContent = "Temperature: " + Math.floor(data.list[7].main.temp) + " °C";
+        forecastWindSpeed.textContent = "Wind Speed: " + data.list[7].wind.speed;
+        forecastHumidity.textContent = "Humidity: " + data.list[7].main.humidity + "%";
+
+    }
+    );
 }
+
+
+
+
+
+
+
+
+
+    
+
 
 function formSubmitHandler(event) {
     event.preventDefault();
@@ -44,6 +83,8 @@ function formSubmitHandler(event) {
     }
 
 };
+
+
 form.addEventListener('submit', formSubmitHandler)
     
     
