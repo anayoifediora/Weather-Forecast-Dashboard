eb4f4b491d8fa01fe676053be5a05a8335c545e9
Kanayo <anayoifediora@live.com>
2023-05-11 22:14:43 +0930

    completed the Readme file

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,3 +1,5 @@
+// DOM query to obtain all required elements
+
 var currentCity = document.querySelector('.current-city');
 var currentTemperature = document.querySelector('.current-temp');
 var currentWindSpeed = document.querySelector('.current-wind');
@@ -8,72 +10,92 @@ var currentDate = document.querySelector('.date');
 var form = document.querySelector('form');
 var formInputEl = document.querySelector('.form-control');
 var cityListDiv = document.querySelector('.d-grid');
+var forecastContainer = document.querySelector('.forecast-container');
+var currentWeatherContainer = document.querySelector('.current-weather');
 
-
-// currentDate.textContent = dayjs().format('DD/MM/YYYY');
+//Displays current date in current weather container
+currentDate.textContent = dayjs().format('YYYY-MM-DD');
 
 let cityName = formInputEl.value.trim();
 
+
 function getCurrentWeather(cityName) {
 
-let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=443ce9d7f610c028f54310f3c2bc25fd'
+        let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=443ce9d7f610c028f54310f3c2bc25fd'
 
-    fetch(requestUrl)
-    .then(function (response) {
-    return response.json();
-    })
-        .then(function (data) {
-        console.log(data);
-        currentCity.textContent = data.name;
-        weatherImageDescription.textContent = data.weather[0].description;
-        currentTemperature.textContent = "Temperature: " + Math.floor(data.main.temp) + " °C";
-        currentWindSpeed.textContent = "Wind Speed: " + data.wind.speed;
-        currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
-        weatherImage.setAttribute("src", 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
+            fetch(requestUrl)
+            .then(function (response) {
+            return response.json();
+            })
+                .then(function (data) {
+                console.log(data);
 
-        
-        }
-        );
-    
-    let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric&appid=443ce9d7f610c028f54310f3c2bc25fd'
-
-    fetch(forecastUrl)
-    .then(function (response) {
-    return response.json();
-    })
-    .then(function (data) {
-    console.log(data);
-
-            // Dom elements for the 5-day forecast
-            
-                var forecastDate = document.querySelectorAll('.card-title')
-                var nameOfCity = document.querySelectorAll('.card-subtitle');
-                var forecastIcon = document.querySelectorAll('.forecast-icon');
-                var forecastDescription = document.querySelectorAll('.forecast-description');
-                var forecastTemperature = document.querySelectorAll('.temperature');
-                var forecastWindSpeed = document.querySelectorAll('.wind');
-                var forecastHumidity = document.querySelectorAll('.humidity');
-
-                foreCastArray = [data.list[7], data.list[15], data.list[23], data.list[31], data.list[39]]
-
-                    for (var i = 0; i < foreCastArray.length; i++) {
-                               
-                    forecastDate[i].textContent = foreCastArray[i].dt_txt;
-                    // nameOfCity[i].textContent = cityName;
-                    forecastIcon[i].setAttribute('src', 'https://openweathermap.org/img/w/' + foreCastArray[i].weather[0].icon + '.png');
-                    forecastDescription[i].textContent = foreCastArray[i].weather[0].description;
-                    forecastTemperature[i].textContent = "Temperature: " + Math.floor(foreCastArray[i].main.temp) + " °C";
-                    forecastWindSpeed[i].textContent = "Wind Speed: " + foreCastArray[i].wind.speed;
-                    forecastHumidity[i].textContent = "Humidity: " + foreCastArray[i].main.humidity + "%";
+                        
+                        currentCity.textContent = data.name;
+                        weatherImageDescription.textContent = data.weather[0].description;
+                        currentTemperature.textContent = "Temperature: " + Math.floor(data.main.temp) + " °C";
+                        currentWindSpeed.textContent = "Wind Speed: " + data.wind.speed;
+                        currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+                        weatherImage.setAttribute("src", 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
+                        
+                        // Sets the data to local storage
+                        currentWeatherData = {
+                            city: data.name,
+                            description: data.weather[0].description,
+                            Temperature: Math.floor(data.main.temp),
+                            windspeed: data.wind.speed,
+                            humidity: data.main.humidity,
+                            image: data.weather[0].icon,
+                        }
+                        localStorage.setItem('currentWeatherData', JSON.stringify(currentWeatherData));
+                
+                
                 }
-        }
-   
-    );
-    displayWeatherInfo(cityName);
-}
+                );
+            
+            let forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric&appid=443ce9d7f610c028f54310f3c2bc25fd'
 
+            fetch(forecastUrl)
+            .then(function (response) {
+            return response.json();
+            })
+            .then(function (forecastData) {
+            console.log(forecastData);
+
+                    // Dom elements for the 5-day forecast
+                    
+                        var forecastDate = document.querySelectorAll('.card-title')
+                        var nameOfCity = document.querySelectorAll('.card-subtitle');
+                        var forecastIcon = document.querySelectorAll('.forecast-icon');
+                        var forecastDescription = document.querySelectorAll('.forecast-description');
+                        var forecastTemperature = document.querySelectorAll('.temperature');
+                        var forecastWindSpeed = document.querySelectorAll('.wind');
+                        var forecastHumidity = document.querySelectorAll('.humidity');
+
+                        // Create a new array with 5-day forecast data for a specific time of the day
+                        foreCastArray = [forecastData.list[7], forecastData.list[15], forecastData.list[23], forecastData.list[31], forecastData.list[39]]
+
+                            for (var i = 0; i < foreCastArray.length; i++) {
+                                    
+                            forecastDate[i].textContent = foreCastArray[i].dt_txt;
+                            // nameOfCity[i].textContent = cityName;
+                            forecastIcon[i].setAttribute('src', 'https://openweathermap.org/img/w/' + foreCastArray[i].weather[0].icon + '.png');
+                            forecastDescription[i].textContent = foreCastArray[i].weather[0].description;
+                            forecastTemperature[i].textContent = "Temperature: " + Math.floor(foreCastArray[i].main.temp) + " °C";
+                            forecastWindSpeed[i].textContent = "Wind Speed: " + foreCastArray[i].wind.speed;
+                            forecastHumidity[i].textContent = "Humidity: " + foreCastArray[i].main.humidity + "%";
+                        }
+                }
+                             
+            );
+            
+            displayWeatherInfo(cityName);
+            currentWeatherContainer.setAttribute('style', 'display:block;');
+            forecastContainer.setAttribute('style', 'display: block;')
+}
+// Displays a button for any city that's searched.
 function displayWeatherInfo(cityName) {
-    var cityButton = document.createElement('button');
+    let cityButton = document.createElement('button');
     cityButton.setAttribute('class', 'btn btn-primary');
     cityButton.textContent = cityName
     cityListDiv.appendChild(cityButton);
@@ -92,6 +114,10 @@ function formSubmitHandler(event) {
 
 };
 
+
 form.addEventListener('submit', formSubmitHandler)
-    
+
+
+
+
     
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Weather-Forecast-Dashboard
+
+## Description
+
+The objective of this project was to develop a Weather Dashboard that will have the capability of displaying the 
+weather information for any city in the world. For any city selected, the dashboard displays the current weather information relating to temperature, wind speed and humidity. It also displays a five-day forecast with the same information for the city. This application was built to enable the user check the weather conditions for possible holiday destinations. 
+
+This project was challenging and I learnt how to properly read, understand and utilize API documentation.
+
+
+
+## Mock-up
+
+The following image displays the functionality of the application:
+
+![landingpage](./Assets/Weather%20Dashboard.png)
+
+## Link to page
+
+The link to the page can be found below
+![Webpage](https://anayoifediora.github.io/Workday-Scheduler/)
+
+    
